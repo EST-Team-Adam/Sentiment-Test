ed0ac7cd87ad23bea499c87b9eeb7bd05458c59e
albertomun <alberto.munisso@gmail.com>
2016-11-03 20:34:05 +0100

    Update main.R

--- Sentiment_test_1.3/Kalman_Filter/main.R
+++ Sentiment_test_1.3/Kalman_Filter/main.R
@@ -9,9 +9,6 @@ rm(list=ls())
 gc()
 source("df_date.r")
 source("kalman_filter.r")
-source("analysis.r")
-source("analysis_pos.r")
-source("analysis_neg.r")
 source("dataframe_manipulator.r")
 
 ## DATA LOADING ##
@@ -21,6 +18,9 @@ df_original<-read.csv("df.csv",sep=';')
 df_original <- timestamp_to_date(df_original)
 wheat$date <- wheat_date(wheat)
 #sentiment_threshold <- 0.2
+
+# Needed for converting compound sentiment to numeric from factor
+df_original$compound <- as.numeric(levels(df_original$compound))[df_original$compound]
 
 ## WHOLE DATA SET ANALYSIS ##
 date1 <- as.Date("2009-07-03")
@@ -33,4 +33,10 @@ date1 <- as.Date("2010-07-03")
 date2 <- as.Date("2011-03-03")
 df2 <- dataframe_aggregator(df_original,date1,date2)
 df <- dataframe_assembler(df2,date1,date2, wheat)
-analysis2010<-analysis(df,date1,date2,sentiment_threshold)
+
+## KALMAN FILTER ##
+
+y <- df$compound
+filtered_sentiment <- kalman_filter(y)
+plot(df$date,y)
+    lines(df$date,ts(filtered_sentiment[[1]]$att[1, ], start = start(filtered_sentiment[[2]]), frequency = frequency(filtered_sentiment[[2]])), col = "red",type="line")
