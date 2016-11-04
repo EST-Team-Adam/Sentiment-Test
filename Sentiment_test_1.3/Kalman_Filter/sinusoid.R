fb876c5b3eb543de06809a81934c9b85e4eb954f
albertomun <alberto.munisso@gmail.com>
2016-11-04 15:38:20 +0100

    Update sinusoid.R

--- Sentiment_test_1.3/Kalman_Filter/sinusoid.R
+++ Sentiment_test_1.3/Kalman_Filter/sinusoid.R
@@ -1,6 +1,9 @@
 ## KALMAN FILTER SINUSOID ##
 
-### Equation: y=a*sin(b*t)+c.unif*amp
+library(FKF)
+source('kalman_filter.r')
+
+### Equation: y=a*sin(b*t)+c.norm*amp
 # variables
 n <- 100 # number of data points
 t <- seq(0,4*pi,,100)
@@ -11,13 +14,13 @@ amp <- 2
 
 # generate data and calculate "y"
 set.seed(1)
-y2 <- a*sin(b*t)+c.norm*amp # Gaussian/normal error
+y <- a*sin(b*t)+c.norm*amp # Gaussian/normal error
 
 # plot results
-plot(t, y2, t="l", ylim=range(y2)*c(1,1.2))
-legend("top", legend=c("y2"), col=1:2, lty=1, ncol=2, bty="n")
+plot(t, y, t="l", ylim=range(y2)*c(1,1.2))
+legend("top", legend=c("y"), col=1:2, lty=1, ncol=2, bty="n")
 
-y <- y2
+y <- y
 filtered_sentiment <- kalman_filter(y)
-plot(t, y2, ylim=range(y2)*c(1,1.2))
+plot(t, y, ylim=range(y)*c(1,1.2), main = 'Noisy sinusoid function with Gaussian generated errors', ylab = 'y', xlab = 'x')
 lines(t,ts(filtered_sentiment[[1]]$att[1, ], start = start(filtered_sentiment[[2]]), frequency = frequency(filtered_sentiment[[2]])), col = "red",type="line")
